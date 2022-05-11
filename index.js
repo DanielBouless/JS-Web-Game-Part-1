92649bc119cff887e6d413f1148a2a0ebe9da5ba
Daniel Bouless <dbouless76@gmail.com>
2022-05-11 18:41:59 -0400

    followed the instructions but the positioning of the newItems is way off

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(location, left, bottom){
+    let image = document.createElement('img')
+image.src = location
+image.style.position = 'fixed'
+image.style.left = left
+image.style.bottom = bottom
+document.body.append(image)
+}
+
+function newItem(location, left, bottom){
+let item = document.createElement('img')
+item.src = location
+item.position = 'fixed'
+item.left = left
+item.bottom = bottom
+document.body.append(item)
+
+item.addEventListener('dblclick', function(){
+    item.remove()
+})
+}
+    
+
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+newItem('assets/sword.png', '500px','405px')
+newItem('assets/shield.png', '165px','185px')
+newItem('assets/staff.png', '600px','100px')
+
